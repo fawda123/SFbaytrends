e1c4cef8f6b1dc2b735d12d05eb9db448addbd7b
fawda123 <mbafs2012@gmail.com>
2018-07-25 17:29:47 -0700

    messing with percent change using baytrends gamDiff function

--- R/funcs.R
+++ R/funcs.R
@@ -155,3 +155,111 @@ dynagam <- function(mod_in, dat_in, cvr, grd = 30, years = NULL, alpha = 1,
   return(p)
   
 }
+
+# estimate percent change trends for selected time periods
+gamdiff <- function(modv, base.yr.set = NA, test.yr.set = NA){
+  
+  # input data used to create model
+  gamdat <- modv$model
+  
+  # get date range, center year
+  por.rng    <- gamdat$dec_time %>% 
+    date_decimal(tz = 'Pacific/Pitcairn') %>% 
+    range
+  por.rng <- lubridate::year(por.rng)
+  
+  centerYear <- gamdat$dec_time %>% 
+    range %>% 
+    mean
+  
+  # estimated values are on the fifteenth of every month for each year of comparisons
+  doy.set <- c(15, 46, 75, 106, 136, 167, 197, 228, 259, 289, 320, 350)
+  
+  # set up base and test years to first two and last two records if base.yr.set
+  # and/or test.yr.set not specified; otherwise concatenate the values
+  if(is.na(base.yr.set[1])) {base.yr.set <-  c(por.rng[1],por.rng[1]+1)}
+  if(is.na(test.yr.set[1])) {test.yr.set <-  c(por.rng[2]-1,por.rng[2])}
+  Nbase.yr <- length(base.yr.set)       # count years in each period
+  Ntest.yr <- length(test.yr.set)
+  yr.set <- c(base.yr.set,test.yr.set)  # combine base years and test years
+  
+  Ndoy  <- length(doy.set) # count predictions per year
+  Nbase <- Ndoy*Nbase.yr   # calculate total predictions in base period
+  Ntest <- Ndoy*Ntest.yr   # calculate total predictions in test period
+  
+  # create a data frame with Nrow rows where Nrow= (Nbase*Ndoy) + (Ntest*Ndoy)...
+  # Nbase yrs of Ndoy baseline dates and Ntest-yrs
+  # of Ndoy current dates. Include: doy, year, logical field (bl) indicating baseline
+  # and current, and centered decimal year (dec_time) using same centering value
+  # computed from data set (centerYear)
+  pdat        <- expand.grid(doy.set,yr.set)       # make df with all comb. of doy.set and yr.set
+  names(pdat) <- c('doy','year')                   # rename variables
+  pdat$bl     <- pdat$year <= base.yr.set[Nbase.yr] # create logical field indicating baseline
+  pdat$dec_time  <- (pdat$year + (pdat$doy-1)/366) # compute dec_time
+  
+  # JBH(24Nov2017): extension of above xa and avg.per.mat
+  #   keeping weight the same--just extending number of values by "*nrow(pdatWgt)"
+  xa <- c(rep(1/Nbase,Nbase),
+          rep(0,Ntest),
+          rep(0,Nbase),
+          rep(1/Ntest,Ntest)) # construct a matrix to average baseline and current periods
+  avg.per.mat <- matrix(xa,nrow=2,ncol=(Nbase+Ntest), byrow=TRUE)
+  
+  # construct matrix to get difference of current minus baseline
+  diff.mat <- c(-1,1)
+  
+  # Extract coefficients (number of terms depends on complexity of GAM formula)
+  beta    <- modv$coefficients        # coefficients vector
+  VCmat   <- modv$Vp                  # variance-covariance matrix of coefficents
+  
+  # Begin calculations to compute differences ####
+  # extract matrix of linear predicters
+  Xpc     <- predict(modv,newdata=pdat,type="lpmatrix")
+  
+  # Compute predictions based on linear predictors (Nrow x 1 matrix)
+  pdep    <- Xpc%*%beta               # equivalent to "predict(gamRslt,newdata=pdatLong)"
+  
+  # Calc. average baseline and average current; stores as a 2x1 matrix
+  period.avg <- avg.per.mat %*% pdep
+  
+  # Calc. average current - average baseline; stores as a 1x1 matrix
+  diff.avg  <- diff.mat %*% period.avg # pre-multiply by differencing matrix to check results
+  
+  # Calc standard errors and confidence intervals on difference predictions
+  xpd       <- diff.mat%*%avg.per.mat%*%Xpc # premultiply linear predictors by averaging and differencing matrices.
+  diff.est  <- xpd%*%beta                   # compute estimate of difference
+  diff.se   <- sqrt(xpd%*%VCmat%*%t(xpd))   # compute Std. Err. by usual rules
+  diff.t    <- diff.est / diff.se
+  diff.pval <- 2*pt(abs(diff.t), modv$df.null, 0, lower.tail = FALSE)
+  
+  #compute CI for differnce
+  alpha <- 0.05
+  halpha    <- alpha/2
+  diff.ci   <- c(diff.est - qnorm(1-halpha) * diff.se,diff.est + qnorm(1-halpha) *diff.se)
+  
+  # back transform mean (03Nov)
+  per.mn.obs <- 10^(period.avg) 
+  
+  # calculate percent change (03Nov)
+  pct.chg <- 100*((per.mn.obs[2] - per.mn.obs[1])/per.mn.obs[1])
+  
+  # difference in obs units (03Nov)
+  diff.est.obs <- per.mn.obs[2] - per.mn.obs[1]
+  
+  # pack up and return results ####
+  gamDiff.tmp <- list(base.yr    = base.yr.set, # years in first baseline set
+                      test.yr    = test.yr.set, # years in second baselin set
+                      per.mn     = as.vector(period.avg), # means by period, analyzed units
+                      per.mn.obs = as.vector(per.mn.obs), # means by period, observed units
+                      pct.chg    = pct.chg, # percent change, back-transformed
+                      diff.est   = diff.est, # difference between periods, analyzed units
+                      diff.est.obs = diff.est.obs, # difference betwwn periods, observed units
+                      diff.se    = diff.se, # standard errors
+                      diff.ci    = diff.ci, # confidence intervals
+                      diff.t     = diff.t, # t stat
+                      diff.pval  = diff.pval # p value
+  )
+  
+  return(gamDiff.tmp)
+  
+}
